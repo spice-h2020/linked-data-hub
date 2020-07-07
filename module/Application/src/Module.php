ac92759e3a23f417b99d66b5b8e99bb01ed52ba0
enridaga <enricodaga@gmail.com>
2020-07-07 18:18:29 +0100

    Initial commit

---
+++ module/Application/src/Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+class Module
+{
+    const VERSION = '0.1.0-SNAPSHOT';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
