8d859cc6b821a34af30edda05fd19fa5e107a007
enridaga <enricodaga@gmail.com>
2020-07-08 09:52:53 +0100

    Added module MKDF\Core and dependencies

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,29 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Paginator',
+    'Zend\Mail',
+    'Zend\Navigation',
+    'Zend\ServiceManager\Di',
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\Prg',
+    'Zend\Session',
+    'Zend\Mvc\I18n',
+    'Zend\Mvc\Console',
+    'Zend\Log',
+    'Zend\Cache',
+    'Zend\Db',
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\I18n',
     'Zend\Router',
     'Zend\Validator',
-    'Application',
+    "MKDF\Core",
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'Application'
 ];
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -57,4 +57,31 @@ return [
             __DIR__ . '/../view',
         ],
     ],
+    // The 'access_filter' key is used by the User module to restrict or permit
+    // access to certain controller actions for unauthenticated visitors.
+    'access_filter' => [
+        'options' => [
+            // The access filter can work in 'restrictive' (recommended) or 'permissive'
+            // mode. In restrictive mode all controller actions must be explicitly listed
+            // under the 'access_filter' config key, and access is denied to any not listed
+            // action for users not logged in. In permissive mode, if an action is not listed
+            // under the 'access_filter' key, access to it is permitted to anyone (even for
+            // users not logged in. Restrictive mode is more secure and recommended.
+            'mode' => 'restrictive'
+        ],
+        'controllers' => [
+            Controller\IndexController::class => [
+                // Allow anyone to visit "index" and "about" actions
+                ['actions' => ['index'], 'allow' => '*'],
+                // Allow authenticated users to visit "settings" action
+                //['actions' => ['settings'], 'allow' => '@']
+            ],
+            Controller\StaticController::class => [
+                // Allow anyone to visit "index" and "about" actions
+                ['actions' => ['documentation'], 'allow' => '*'],
+                // Allow authenticated users to visit "settings" action
+                //['actions' => ['settings'], 'allow' => '@']
+            ],
+        ]
+    ],
 ];
